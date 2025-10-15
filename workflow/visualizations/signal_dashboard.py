40c275b46b1423a5e09cbc568bef4732445eee64
stevenreed <reedsteven397@gmail.com>
2025-10-15 11:25:49 -0500

    Delete workflow/visualizations directory

--- workflow/visualizations/signal_dashboard.py
+++
@@ -1,4 +0,0 @@
-from workflow.visualizations.signal_dashboard import visualize_signal_density, load_coaching_tree
-
-tree = load_coaching_tree("coaching_tree_matrix.json")
-visualize_signal_density(tree)
